69ab015ff4fd68b7621f8f2b40fbde3c7a6ac376
phpusr <phpusr@gmail.com>
2014-04-02 13:41:41 +0600

    add: Prime

---
+++ src/main/scala/prime/Prime.scala
@@ -0,0 +1,37 @@
+package prime
+
+/**
+ * @author phpusr
+ *         Date: 02.04.14
+ *         Time: 13:27
+ */
+
+/**
+ * Утилита для генерации простых чисел
+ */
+object Prime {
+
+  private val debugEnable = false
+
+  /** Генерация простого числа */
+  def generatePrime(maxNumber: Int) = {
+    var number = 2
+    do {
+      number = Math.round(Math.random()*maxNumber+1).toInt
+    } while(!isPrime(number))
+
+    number
+  }
+
+  /** Проверка, является ли число простым */
+  def isPrime(number: Int) = {
+    if (number == 1) false
+    else {
+      val res = for (i <- 2 to number/2 if number % i == 0) yield i
+      if (debugEnable) println(number + ": " + res.mkString(", "))
+
+      res.size == 0
+    }
+  }
+
+}
---
+++ src/main/scala/prime/TestPrime.scala
@@ -0,0 +1,24 @@
+package prime
+
+/**
+ * @author phpusr
+ *         Date: 02.04.14
+ *         Time: 13:21
+ */
+
+object TestPrime extends App {
+
+  testIsPrime()
+
+  def testIsPrime() {
+    assert(!Prime.isPrime(100))
+    assert(!Prime.isPrime(21))
+    assert(!Prime.isPrime(25))
+    assert(!Prime.isPrime(105))
+
+    assert(Prime.isPrime(23))
+    assert(Prime.isPrime(3))
+    assert(Prime.isPrime(103))
+    assert(Prime.isPrime(127))
+  }
+}
