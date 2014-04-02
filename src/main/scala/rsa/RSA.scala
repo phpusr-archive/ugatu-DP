4552ae0bf55594b7c3837ae65417788ff3e9d3c6
phpusr <phpusr@gmail.com>
2014-04-02 12:51:30 +0600

    add: Код генерации ключей

--- src/main/scala/rsa/RSA.scala
+++ src/main/scala/rsa/RSA.scala
@@ -1,5 +1,7 @@
 package rsa
 
+import euclide.Euclide
+
 /**
  * @author phpusr
  *         Date: 02.04.14
@@ -10,5 +12,38 @@ package rsa
  * Алгоритм шифрования RSA (Rivest, Shamir и Adleman)
  */
 object RSA {
+
+  /** Генерация ключей */
+  def generateKeys() {
+    val (p, q) = generatePQ()
+    val n = p * q
+    val phi = getPhi(p, q) //TODO
+    val publicKey = generatePublicKey()
+    val privateKey = generatePrivateKey(p, q, publicKey)
+
+    (n, publicKey, privateKey)
+  }
+
+  def generatePublicKey() = {
+    10 //TODO
+  }
+
+  /** Генерация открытого ключа по известным p и q */
+  def generatePrivateKey(p: Int, q: Int, e: Int) = {
+    val phi = getPhi(p, q)
+
+    val gcd = Euclide.gcdExt(e, phi)
+    assert(e * gcd.x + phi * gcd.y == gcd.d, "Wrong d")
+
+    gcd.x
+  }
+
+  /** Генерация чисел P и Q */
+  def generatePQ() = {
+    (1, 2) //TODO
+  }
+
+  /** Возвращает число Фи */
+  private def getPhi(p: Int, q: Int) = (p-1) * (q-1)
 
 }
