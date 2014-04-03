e12f7394611031d5de6a14fc14d596530e11d392
phpusr <phpusr@gmail.com>
2014-04-03 15:19:59 +0600

    add: Logger

---
+++ src/main/scala/org/dyndns/phpusr/util/log/Logger.scala
@@ -0,0 +1,26 @@
+package org.dyndns.phpusr.util.log
+
+/**
+ * @author phpusr
+ *         Date: 03.04.14
+ *         Time: 14:54
+ */
+
+/**
+ * Логгер
+ */
+trait LoggerMixin {
+  private val infoEnable = true
+  private val debugEnable = true
+  private val traceEnable = false
+
+  def title(s: String) { if (infoEnable) println(s"\n----- $s -----") }
+  def log(s: String) { if (debugEnable) println(s"LOG:: $s") }
+  def trace(s: String) { if (traceEnable) println(s"TRACE:: $s") }
+}
+
+class Logger extends LoggerMixin
+
+object Logger {
+  def apply() = new Logger
+}
