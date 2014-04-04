3bf6f39acdac9b2fa6f50b2254f666dadb4cfa14
phpusr <phpusr@gmail.com>
2014-04-04 20:27:14 +0600

    add: Main

---
+++ src/main/scala/dataprotection/lab/three/run/Main.scala
@@ -0,0 +1,21 @@
+package dataprotection.lab.three.run
+
+import dataprotection.lab.three.form.MainForm
+import javax.swing.UIManager
+
+
+/**
+ * @author phpusr
+ *         Date: 03.04.14
+ *         Time: 15:33
+ */
+
+/**
+ * Запуск программы
+ */
+object Main extends App {
+  // Установка темы оформления
+  UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel")
+  // Создание и запуск главной формы
+  MainForm.main(args)
+}
