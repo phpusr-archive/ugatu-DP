50ab187cc45bba94030aadd24e4d8e6a8fed2ffd
phpusr <phpusr@gmail.com>
2014-04-09 20:25:58 +0600

    change: TestPrime to PrimeSpec

--- src/test/scala/dataprotection/lab/three/prime/PrimeSpec.scala
+++ src/test/scala/dataprotection/lab/three/prime/PrimeSpec.scala
@@ -1,24 +1,30 @@
 package dataprotection.lab.three.prime
 
+import org.scalatest.FlatSpec
+
 /**
- * @author phpusr
  *         Date: 02.04.14
  *         Time: 13:21
  */
 
-object TestPrime extends App {
+/**
+ * Тестирование класса Prime
+ */
+class PrimeSpec extends FlatSpec {
 
-  testIsPrime()
-
-  def testIsPrime() {
+  "This numbers dont't" should "be prime" in {
     assert(!Prime.isPrime(100))
     assert(!Prime.isPrime(21))
     assert(!Prime.isPrime(25))
     assert(!Prime.isPrime(105))
+  }
 
+
+  "This number" should "be prime" in {
     assert(Prime.isPrime(23))
     assert(Prime.isPrime(3))
     assert(Prime.isPrime(103))
     assert(Prime.isPrime(127))
   }
+
 }
