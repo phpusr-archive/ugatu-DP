843f998977ed882772bfa2b21fcb60178adb4e97
phpusr <phpusr@gmail.com>
2014-04-21 12:52:32 +0600

    add: GostTopPanel

---
+++ src/main/scala/dataprotection/lab/common/gui/form/main/panel/top/GostTopPanel.scala
@@ -0,0 +1,14 @@
+package dataprotection.lab.common.gui.form.main.panel.top
+
+/**
+ * @author phpusr
+ *         Date: 21.04.14
+ *         Time: 12:35
+ */
+
+/**
+ * Панель для шифрования методом ГОСТ-28147-89
+ */
+trait GostTopPanel {
+
+}
