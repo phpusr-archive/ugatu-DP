bce50894f85b94c085642a0e2231f61a3a5b0503
phpusr <phpusr@gmail.com>
2014-06-02 14:33:36 +0600

    add: Тест сравнения с библиотечным

--- src/test/scala/dataprotection/rgr/idea/IDEA$Spec.scala
+++ src/test/scala/dataprotection/rgr/idea/IDEA$Spec.scala
@@ -2,6 +2,8 @@ package dataprotection.rgr.idea
 
 import org.scalatest.FlatSpec
 import org.dyndns.phpusr.bitnumber.BitNumber
+import org.bouncycastle.crypto.params.KeyParameter
+import org.bouncycastle.crypto.engines.IDEAEngine
 
 /**
  * @author phpusr
@@ -14,10 +16,11 @@ import org.dyndns.phpusr.bitnumber.BitNumber
  */
 class IDEA$Spec extends FlatSpec {
 
+  /** Преобразование BitNumber в удобочитаемый вид */
+  val toHex = (number: BitNumber) => number.toHexStr.sliding(4, 4).mkString(", ")
+
   /** Отладка шифрования и расшифрования */
   it should "debug" in {
-
-    val toHex = (number: BitNumber) => number.toHexStr.sliding(4, 4).mkString(", ")
 
     val keyArray = Array(0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8).map(_.toByte)
     val key = BitNumber(keyArray)
@@ -41,6 +44,52 @@ class IDEA$Spec extends FlatSpec {
 
     assert(data.toString == dec.toString)
 
+  }
+
+  /** Сравнение с библиотечным */
+  it should "equals with lib" in {
+    println("\nEquals with lib")
+
+    println("\nEncrypt")
+
+    // Шифрование библиотекой
+    val keyArray = Array(0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8).map(_.toByte)
+    val keyParam = new KeyParameter(keyArray)
+
+    val engine = new IDEAEngine
+    engine.init(true, keyParam)
+
+    val dataArray = Array(0, 0, 0, 1, 0, 2, 0, 3).map(_.toByte)
+    val encryptArray = new Array[Byte](dataArray.size)
+    engine.processBlock(dataArray, 0, encryptArray, 0)
+    val encryptBitsLib = BitNumber(encryptArray)
+    println("lib: " + toHex(encryptBitsLib))
+
+    // Шифрование моим классом
+    val key = BitNumber(keyArray)
+    val idea = new IDEA(key, true)
+    val data = BitNumber(dataArray)
+    val encryptBits = idea.processBlocks(data)
+    println("my : " + toHex(encryptBits))
+
+    assert(encryptBitsLib == encryptBits)
+
+    println("\nDecrypt")
+
+    // Расшифрование библиотекой
+    engine.init(false, keyParam)
+    val decryptArray = new Array[Byte](dataArray.size)
+    engine.processBlock(encryptArray, 0, decryptArray, 0)
+    val decryptBitsLib = BitNumber(decryptArray)
+    println("lib: " + toHex(decryptBitsLib))
+
+    // Расшифрование моим классом
+    val decryptIdea = new IDEA(key, false)
+    val decryptBits = decryptIdea.processBlocks(encryptBits)
+    println("my : " + toHex(decryptBits))
+
+    assert(decryptBitsLib == decryptBits)
+    assert(decryptBits == data)
   }
 
 }
